38f89efc12be77b52614151ad55539fe22feb45f
paralixo <33687200+paralixo@users.noreply.github.com>
2018-11-15 16:25:43 +0100

    tout dans une seule commande

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ program
     .option('-s, --search', 'Search a movie by name and show some informations')
     .option('-n, --now', 'Get now playing movies')
     .option('-i, --info [id]', 'Get detailed informations from a movie id')
+    .option('-a, --actor', 'Show description of the actor')
 // On parse (convertit en format utilisable) les options
 // fonction synchrone
 program.parse(process.argv)
@@ -112,6 +113,44 @@ if (program.search) {
         })
         .catch(function (error) {
             console.log(error)
+        })
+    
+} else if (program.actor){
+   
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Entrez le nom de l\'acteur ou de l\'actrice',
+            name: 'actorname'
+         }
+        ]).then((answers) => {
+            const link = "https://api.themoviedb.org/3/search/person?api_key="+api_key+'&query='+answers['actorname']
+            axios.get(link)
+            .then(function (response) {
+                list=[]
+                for (result of response.data.results) {
+                    list.push(result['name']+" | "+result['known_for'][0]['title'])
+                }
+                
+                inquirer.prompt([
+                    {
+                        type: 'rawlist',
+                        message: 'Quel acteur souhaitez-vous choisir ? (' + list.length + ' résultats)',
+                        name: 'film',
+                        choices: list
+                    }
+                ]).then((answers) => {
+                    answers = answers['film']
+                    nom = answers.substr(0, answers.indexOf("|")-1)
+                    known = answers.substr(answers.indexOf("|")+2)
+                    for (result of response.data.results) {
+                        if (result['name'] == nom && result['known_for'][0]['title'] == known ) {
+                            console.log(result)
+                        }
+                    }
+                })
+                
+            } ).catch(function (error) {console.log(error)}) 
         })
     
 } else {
